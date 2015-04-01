60f5844dfaa4070daab2360f5ef5e7b1a4de3535
Paul Von Schrottky <paulvonschrottky@gmail.com>
2015-04-01 07:45:42 -0400

    Initial Commit

---
+++ TestShrinkingNavBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestShrinkingNavBar
+//
+//  Created by Paul Von Schrottky on 4/1/15.
+//  Copyright (c) 2015 Paul Von Schrottky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
